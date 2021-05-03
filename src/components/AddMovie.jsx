557dcfd767053e0571908fe0d0af48537f69f9cb
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-03 19:02:05 -0300

    Resolução do requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  /* constructor(props) {
-    super(props) {
+  constructor(props) {
+    super(props);
 
-    }
-  } */
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label">
           Título
-          <input type="text" data-testid="title-input" />
+          <input 
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ title }
+          />
         </label>
+        <labe data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            valor={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ subtitle }
+          />
+        </labe>
       </form>
     );
   }
 }
+
+/* AddMovie.propTypes = {
+  subtitle: PropTypes.string.isRequired,
+} */
 
 export default AddMovie;
