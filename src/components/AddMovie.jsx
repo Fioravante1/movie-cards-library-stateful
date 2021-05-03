74344cff38ca42a977bb7902ed69406806a7bcfc
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-03 18:04:50 -0300

    Resolução do requisito 6

---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  /* constructor(props) {
+    super(props) {
+
+    }
+  } */
+  render() {
+    return (
+      <h1>Fioravante</h1>
+    );
+  }
+}
+
+export default AddMovie;
