96804c98f171a2ad5ebd5a6950316c328c4521eb
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-04 18:38:11 -0300

    Cria componente movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,41 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
-import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: props.movies,
-    };
-  }
-
   render() {
-    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div>
-        <SearchBar
-          searchText={ searchText }
-          bookmarkedOnly={ bookmarkedOnly }
-          selectedGenre={ selectedGenre }
-        />
-        <MovieList />
-        <AddMovie movie={ movies } />
-      </div>
+      <SearchBar />
     );
   }
 }
-
-MovieLibrary.propTypes = {
-  movies: PropTypes.shape({
-    movies: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export default MovieLibrary;
