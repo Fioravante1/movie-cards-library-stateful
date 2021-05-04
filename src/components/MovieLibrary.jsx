c2a26cfb017b345247fac118cd49bf25ab83fae0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-04 19:17:42 -0300

    renderiza os componentes search e MovieList para movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <SearchBar />
+      <div>
+
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+
+        <MovieList movies={ movies } />
+      </div>
+
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    movies: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieLibrary;
