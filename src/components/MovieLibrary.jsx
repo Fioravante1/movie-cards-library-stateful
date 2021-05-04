af961fae14118e5bc714a1ff815ba2566d4d4808
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-04 14:51:34 -0300

    Tentativa de resolução dos requisitos 15 16 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <h1>Fiori</h1>
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList />
+        <AddMovie mavie={ movies } />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    movies: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieLibrary;
